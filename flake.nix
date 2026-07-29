b8f547541927b495e74eef8fcf7798fb2b83429d
Reach <reach@42paris.fr>
2026-07-29 14:30:06 +0200

    build(nix): add flake.nix with devShell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A basic Nix flake with Go devShell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gnumake
+            go
+            golangci-lint
+            gopls
+            gotools
+            tree-sitter
+            tree-sitter-grammars.tree-sitter-go
+          ];
+        };
+      }
+    );
+}
